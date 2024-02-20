b2a5304060b045e8a2daa506282e1381556dfc98
eljamm <fedi.jamoussi@protonmail.ch>
2024-02-20 20:50:33 +0100

    Refactor shapes test for structs

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,6 +2,10 @@ package main
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,23 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		want := 72.0
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 
 		if got != want {
 			t.Errorf("Got '%g', want '%g'", got, want)
 		}
+	}
+
+	t.Run("Rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("Circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("Got '%g', want '%g'", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
