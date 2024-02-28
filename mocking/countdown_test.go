cc4323dbae64837732cd50d933627e01664e907d
eljamm <fedi.jamoussi@protonmail.ch>
2024-02-28 18:41:10 +0100

    Write example for dependency injection mocking

---
+++ mocking/countdown_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+	if got != want {
+		t.Errorf("Got %q, want %q", got, want)
+	}
+	if spySleeper.Calls != 3 {
+		t.Errorf("Not enough calls to sleeper, want 3, got %d", spySleeper.Calls)
+	}
+}
---
+++ mocking/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type (
+	DefaultSleeper struct{}
+	SpySleeper     struct {
+		Calls int
+	}
+)
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
