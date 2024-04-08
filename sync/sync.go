6eb210cab0fdaef5da9a2a37fbdf9a7b8673c523
eljamm <fedi.jamoussi@protonmail.ch>
2024-04-08 13:17:07 +0100

    Write tests for syncing goroutines with locks

---
+++ sync/sync.go
@@ -0,0 +1,22 @@
+package sync
+
+import "sync"
+
+type Counter struct {
+	value int
+	mu    sync.Mutex
+}
+
+func NewCounter() *Counter {
+  return &Counter{}
+}
+
+func (c *Counter) Inc() {
+  c.mu.Lock()
+  defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
